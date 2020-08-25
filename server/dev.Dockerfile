bc07fa973deb53316fa8f3f4b1a6e83e745a6138
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-08-25 18:31:13 +0530

    Image Processing Complete

--- server/dev.Dockerfile
+++ server/dev.Dockerfile
@@ -2,5 +2,8 @@ FROM tiangolo/uvicorn-gunicorn:python3.6-alpine3.8
 RUN mkdir -p /app
 COPY requirements.txt /app
 RUN pip install --no-cache-dir -r requirements.txt
+RUN apk add build-base python-dev py-pip jpeg-dev zlib-dev
+ENV LIBRARY_PATH=/lib:/usr/lib
+RUN pip install pillow
 COPY . /app
 ENTRYPOINT ["/start-reload.sh"]
--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
 
 app = FastAPI(root_path="/api/")
 app.add_middleware(
@@ -11,4 +13,8 @@ app.add_middleware(
 @app.post("/style")
 async def create_file(content_file: bytes = File(...), style_file: bytes = File(...)):
     print(type(content_file), type(style_file))
+    print(content_file[:10])
+    content_image = Image.open(io.BytesIO(content_file))
+    style_image = Image.open(io.BytesIO(style_file))
+    print("Content: {}, Style: {}".format(content_image.size, style_image.size))
     return {"file_size": len(style_file)}
